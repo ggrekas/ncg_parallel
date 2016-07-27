d1283d9ead741bea3291cb39aef2046c92b5c06d
Giorgos Grekas <grekas.g@gmail.com>
2016-07-27 20:40:42 +0300

    almost compatible version of parallelized ncg and line search

--- myPar_line_search.py
+++ myPar_line_search.py
@@ -1,3 +1,6 @@
+# TODO: account for memory leaks
+
+
 def line_search_wolfe1(f, fprime, xk, pk, gfk=None,
                        old_fval=None, old_old_fval=None,
                        args=(), c1=1e-4, c2=0.9, amax=50, amin=1e-8,
@@ -51,18 +54,24 @@ gval : array
         fc[0] += 1
         xk_c = xk.copy()
         xk_c.axpy(s, pk)
-        return f(xk_c, *args) # modify me
+        f_val = f(xk_c, *args)
+        del xk_c
+        return  f_val# modify me
 
 
     def derphi(s):
         xk_c = xk.copy()
         xk_c.axpy(s, pk)
         gval[0] = fprime(xk_c, *newargs) # modify me
+        del xk_c
+
         if gradient:
             gc[0] += 1
         else:
             fc[0] += len(xk) + 1 # modify me
-        return gval[0].inner(pk)#np.dot(gval[0], pk) # modify me
+        # d_val = gval[0].inner(pk)  # np.dot(gval[0], pk) # modify me
+        # del gval[0]
+        return gval[0].inner(pk) #d_val
 
 
     derphi0 = gfk.inner(pk) #np.dot(gfk, pk) # modify me
@@ -149,5 +158,239 @@ def scalar_search_wolfe1(phi, derphi, phi0=None, old_phi0=None, derphi0=None,
 
     return stp, phi1, phi0
 
+#------------------------------------------------------------------------------
+# Pure-Python Wolfe line and scalar searches
+#------------------------------------------------------------------------------
 
-line_search = line_search_wolfe1
+def line_search_wolfe2(f, myfprime, xk, pk, gfk=None, old_fval=None,
+                       old_old_fval=None, args=(), c1=1e-4, c2=0.9, amax=50):
+    """Find alpha that satisfies strong Wolfe conditions.
+    Parameters
+    ----------
+    f : callable f(x,*args)
+        Objective function.
+    myfprime : callable f'(x,*args)
+        Objective function gradient.
+    xk : ndarray
+        Starting point.
+    pk : ndarray
+        Search direction.
+    gfk : ndarray, optional
+        Gradient value for x=xk (xk being the current parameter
+        estimate). Will be recomputed if omitted.
+    old_fval : float, optional
+        Function value for x=xk. Will be recomputed if omitted.
+    old_old_fval : float, optional
+        Function value for the point preceding x=xk
+    args : tuple, optional
+        Additional arguments passed to objective function.
+    c1 : float, optional
+        Parameter for Armijo condition rule.
+    c2 : float, optional
+        Parameter for curvature condition rule.
+    amax : float, optional
+        Maximum step size
+    Returns
+    -------
+    alpha : float or None
+        Alpha for which ``x_new = x0 + alpha * pk``,
+        or None if the line search algorithm did not converge.
+    fc : int
+        Number of function evaluations made.
+    gc : int
+        Number of gradient evaluations made.
+    new_fval : float or None
+        New function value ``f(x_new)=f(x0+alpha*pk)``,
+        or None if the line search algorithm did not converge.
+    old_fval : float
+        Old function value ``f(x0)``.
+    new_slope : float or None
+        The local slope along the search direction at the
+        new value ``<myfprime(x_new), pk>``,
+        or None if the line search algorithm did not converge.
+    Notes
+    -----
+    Uses the line search algorithm to enforce strong Wolfe
+    conditions.  See Wright and Nocedal, 'Numerical Optimization',
+    1999, pg. 59-60.
+    For the zoom phase it uses an algorithm by [...].
+    """
+    fc = [0]
+    gc = [0]
+    gval = [None]
+
+
+    def phi(alpha):
+        fc[0] += 1
+        xk_c = xk.copy()
+        xk_c.axpy(alpha, pk)
+        f_val = f(xk_c, *args)
+        del xk_c
+        return f_val  # modify me
+
+    if isinstance(myfprime, tuple):
+        def derphi(alpha):
+            fc[0] += xk.size() + 1
+            eps = myfprime[1]
+            fprime = myfprime[0]
+            newargs = (f, eps) + args
+
+            xk_c = xk.copy()
+            xk_c.axpy(s, pk)
+            gval[0] = fprime(xk + alpha * pk, *newargs)  # store for later use
+
+            # d_val = gval[0].inner(pk)  # np.dot(gval[0], pk) # modify me
+            # del gval[0]
+            return gval[0].inner(pk)  #d_val
+    else:
+        fprime = myfprime
+
+        def derphi(alpha):
+            gc[0] += 1
+
+            xk_c = xk.copy()
+            xk_c.axpy(s, pk)
+            gval[0] = fprime(xk + alpha * pk, *args)  # store for later use
+            del xk_c
+
+            # d_val = gval[0].inner(pk)  # np.dot(gval[0], pk) # modify me
+            # del gval[0]
+            return gval[0].inner(pk) # d_val
+
+
+    if gfk is None:
+        gfk = fprime(xk, *args)
+    derphi0 = gfk.inner(pk) #np.dot(gfk, pk)
+
+    alpha_star, phi_star, old_fval, derphi_star = scalar_search_wolfe2(
+            phi, derphi, old_fval, old_old_fval, derphi0, c1, c2, amax)
+
+    if derphi_star is None:
+        warn('The line search algorithm did not converge', LineSearchWarning)
+    else:
+        # derphi_star is a number (derphi) -- so use the most recently
+        # calculated gradient used in computing it derphi = gfk*pk
+        # this is the gradient at the next step no need to compute it
+        # again in the outer loop.
+        derphi_star = gval[0] # ????????????????????????? tha prepei na vgalei
+        #  lathos logw del gval[0]
+
+    return alpha_star, fc[0], gc[0], phi_star, old_fval, derphi_star
+
+
+def scalar_search_wolfe2(phi, derphi=None, phi0=None,
+                         old_phi0=None, derphi0=None,
+                         c1=1e-4, c2=0.9, amax=50):
+    """Find alpha that satisfies strong Wolfe conditions.
+    alpha > 0 is assumed to be a descent direction.
+    Parameters
+    ----------
+    phi : callable f(x)
+        Objective scalar function.
+    derphi : callable f'(x), optional
+        Objective function derivative (can be None)
+    phi0 : float, optional
+        Value of phi at s=0
+    old_phi0 : float, optional
+        Value of phi at previous point
+    derphi0 : float, optional
+        Value of derphi at s=0
+    c1 : float, optional
+        Parameter for Armijo condition rule.
+    c2 : float, optional
+        Parameter for curvature condition rule.
+    amax : float, optional
+        Maximum step size
+    Returns
+    -------
+    alpha_star : float or None
+        Best alpha, or None if the line search algorithm did not converge.
+    phi_star : float
+        phi at alpha_star
+    phi0 : float
+        phi at 0
+    derphi_star : float or None
+        derphi at alpha_star, or None if the line search algorithm
+        did not converge.
+    Notes
+    -----
+    Uses the line search algorithm to enforce strong Wolfe
+    conditions.  See Wright and Nocedal, 'Numerical Optimization',
+    1999, pg. 59-60.
+    For the zoom phase it uses an algorithm by [...].
+    """
+
+    if phi0 is None:
+        phi0 = phi(0.)
+
+    if derphi0 is None and derphi is not None:
+        derphi0 = derphi(0.)
+
+    alpha0 = 0
+    if old_phi0 is not None and derphi0 != 0:
+        alpha1 = min(1.0, 1.01*2*(phi0 - old_phi0)/derphi0)
+    else:
+        alpha1 = 1.0
+
+    if alpha1 < 0:
+        alpha1 = 1.0
+
+    if alpha1 == 0:
+        # This shouldn't happen. Perhaps the increment has slipped below
+        # machine precision?  For now, set the return variables skip the
+        # useless while loop, and raise warnflag=2 due to possible imprecision.
+        alpha_star = None
+        phi_star = phi0
+        phi0 = old_phi0
+        derphi_star = None
+
+    phi_a1 = phi(alpha1)
+    #derphi_a1 = derphi(alpha1)  evaluated below
+
+    phi_a0 = phi0
+    derphi_a0 = derphi0
+
+    i = 1
+    maxiter = 10
+    for i in xrange(maxiter):
+        if alpha1 == 0:
+            break
+        if (phi_a1 > phi0 + c1 * alpha1 * derphi0) or \
+           ((phi_a1 >= phi_a0) and (i > 1)):
+            alpha_star, phi_star, derphi_star = \
+                        _zoom(alpha0, alpha1, phi_a0,
+                              phi_a1, derphi_a0, phi, derphi,
+                              phi0, derphi0, c1, c2)
+            break
+
+        derphi_a1 = derphi(alpha1)
+        if (abs(derphi_a1) <= -c2*derphi0):
+            alpha_star = alpha1
+            phi_star = phi_a1
+            derphi_star = derphi_a1
+            break
+
+        if (derphi_a1 >= 0):
+            alpha_star, phi_star, derphi_star = \
+                        _zoom(alpha1, alpha0, phi_a1,
+                              phi_a0, derphi_a1, phi, derphi,
+                              phi0, derphi0, c1, c2)
+            break
+
+        alpha2 = 2 * alpha1   # increase by factor of two on each iteration
+        i = i + 1
+        alpha0 = alpha1
+        alpha1 = alpha2
+        phi_a0 = phi_a1
+        phi_a1 = phi(alpha1)
+        derphi_a0 = derphi_a1
+
+    else:
+        # stopping test maxiter reached
+        alpha_star = alpha1
+        phi_star = phi_a1
+        derphi_star = None
+        warn('The line search algorithm did not converge', LineSearchWarning)
+
+    return alpha_star, phi_star, phi0, derphi_star
+
